20ae3fd0e8e158bfe8eabc1f150429caed42846e
Frederick Imanuel Setiawan <fred4tech@gmail.com>
2020-10-27 13:26:52 +0700

    test-css2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         
         {{-- Import CSS --}}
-        <link type="text/css" rel="stylesheet" href="{{URL::asset('css/indexstyle.css')}}">
+        <link type="text/css" rel="stylesheet" href="{{asset('css/indexstyle.css')}}">
 
         {{-- Import Rubik Font --}}
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
