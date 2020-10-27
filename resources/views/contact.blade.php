0d898fe3674e67f101c40cf797417ad3c56269a7
Frederick Imanuel Setiawan <fred4tech@gmail.com>
2020-10-27 17:02:13 +0700

    add-contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -51,6 +51,8 @@
             </div>
         </div>
         
+
+        {{-- Content --}}
         <div class="container mt-5">
             <div class="row">
                 <div class="col-md-2"></div>
@@ -61,12 +63,29 @@
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu ante vitae purus consectetur tempor. Etiam eleifend in leo eget molestie. Donec diam eros, rutrum at ultrices in, tristique eu ante. Proin vitae purus molestie ipsum dapibus consectetur tempor vel ipsum. Suspendisse ullamcorper a justo nec suscipit. Nunc finibus libero sed augue auctor venenatis. Duis scelerisque tristique quam eget porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris nulla enim, tincidunt eget eros laoreet, porta dictum lorem. Proin eleifend, velit et ullamcorper egestas, odio tortor hendrerit metus, at rhoncus neque felis faucibus dui. Aenean ante nisl, pharetra sit amet aliquet sit amet, efficitur eu purus. Quisque sodales viverra ex, nec mattis velit sodales quis. Nullam quis erat sit amet risus condimentum venenatis non ut dui. Maecenas faucibus, nulla porttitor venenatis tristique, augue quam dapibus ligula, sit amet bibendum elit risus ac dolor.
                     </div>
                     
-
+                    <div class="container mt-3 contact-link">
+                        <div class="row">
+                            <a href="https://www.linkedin.com/in/frederickimanuels/" target="_blank">Linkedin</a>
+                        </div>
+                        <div class="row mt-1">
+                            <a href="https://github.com/frederickimanuels" target="_blank">Github</a>
+                        </div>
+                        <div class="row mt-1">
+                            <a href="https://instagram.com/frederick_imanuel" target="_blank">Instagram</a>
+                        </div>
+                        <div class="row mt-2">
+                            <button class="btn btn-outline-primary copy-email" data-clipboard-text="frederickimanuels@gmail.com">
+                                <span>frederickimanuels@gmail.com</span>
+                            </button>
+                        </div>
+                        
+                        
+                    </div>
+                    
                 </div>
                 <div class="col-md-2"></div>
             </div>
         </div>
-
 
         {{-- Content --}}
         
@@ -82,5 +101,14 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
         -->
+
+
+
+        {{-- Copy to Clipboard --}}
+        <script src="{{ url('js/clipboard.min.js')}}"></script>
+        <script>
+            new ClipboardJS('.btn');
+        </script>
     </body>
 </html>
+
